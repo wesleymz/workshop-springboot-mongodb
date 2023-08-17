cf123b719e88f5eb219f306505c8d09c07b82650
Wesley Santos Silva (SYSMAP) <wesleysilva.sysmap@latam.com>
2023-08-17 17:30:28 -0300

    DelAtualização de usuário com PUT

--- src/main/java/com/wesleysantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wesleysantos/workshopmongo/resources/UserResource.java
@@ -45,4 +45,12 @@ public class UserResource {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO userDTO) {
+        User user = service.fromDTO(userDTO);
+        user.setId(id);
+        service.update(user);
+        return ResponseEntity.noContent().build();
+    }
 }
