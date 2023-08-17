543e2656757bb45e329ed2443e230f2047e31ae5
Wesley Santos Silva (SYSMAP) <wesleysilva.sysmap@latam.com>
2023-08-17 16:43:03 -0300

    UsObtendo um usuário por id

--- src/main/java/com/wesleysantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wesleysantos/workshopmongo/resources/UserResource.java
@@ -5,9 +5,7 @@ import com.wesleysantos.workshopmongo.dto.UserDTO;
 import com.wesleysantos.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,10 +17,16 @@ public class UserResource {
     @Autowired
     private UserService service;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
         List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
---
+++ src/main/java/com/wesleysantos/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.wesleysantos.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
