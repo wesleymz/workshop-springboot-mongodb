4295b9dd562b084572c7b8744c63e65a1f637374
Wesley Santos Silva (SYSMAP) <wesleysilva.sysmap@latam.com>
2023-08-18 12:45:46 -0300

    Consulta simples com query methods

--- src/main/java/com/wesleysantos/workshopmongo/resources/PostResource.java
+++ src/main/java/com/wesleysantos/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.wesleysantos.workshopmongo.resources;
 
 import com.wesleysantos.workshopmongo.domain.Post;
+import com.wesleysantos.workshopmongo.resources.util.URL;
 import com.wesleysantos.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,5 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = service.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> posts = service.findByTitle(text);
+        return ResponseEntity.ok().body(posts);
     }
 }
